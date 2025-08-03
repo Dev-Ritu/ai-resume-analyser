3a81691986d5a18d7acbc004c4b154c4d6f1a874
Ritu <ritikasinha9958@gmail.com>
2025-08-03 18:31:59 +0530

    Added Navbar and the herosection with Resume Cards layout with header

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,34 @@
 import type { Route } from "./+types/home";
 import { Welcome } from "../welcome/welcome";
+import Navbar from "~/components/Navbar";
+import { resumes } from "../../constants";
+import ResumeCard from "~/components/ResumeCard";
+import type { Key } from "react";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "Resume with AI" },
+    { name: "description", content: "Get Smart feedback fro your dream job!" },
   ];
 }
 
 export default function Home() {
-  return <Welcome />;
+  return (
+    <main className="bg-[url('/images/bg-main.svg')] bg-cover">
+      <Navbar />
+      <section className="main-section">
+        <div className="page-heading">
+          <h1>Track Your Applictaions & Resume Ratings</h1>
+          <h2>Review Your Submissions and check AI-Powered Feedback.</h2>
+        </div>
+      </section>
+      {resumes.length > 0 && (
+        <div className="resumes-section">
+          {resumes.map((resume: { id: Key | null | undefined; }) => (
+            <ResumeCard key={resume.id} resume={resume} />
+          ))}
+        </div>
+      )}
+    </main>
+  );
 }
